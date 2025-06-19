a7a9164dbc2a8f92162f407d692b54db26a9ef29
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-06-19 21:20:19 +0600

    existing projects has been setup

--- index.js
+++ index.js
@@ -10,12 +10,10 @@ app.use(express.json());
 
 
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
